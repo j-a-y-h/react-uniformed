9914712f82f1456c1bccd3256d209db7fa8f36bb
Jay Harris <jay@sourware.com>
2019-06-16 20:20:52 -0400

    Added initial argument

--- src/useFields.ts
+++ src/useFields.ts
@@ -1,2 +1,34 @@
-export function useFields() {
+import React from "react";
+export interface Fields {
+    /**
+     * The key is the name of the field
+     * The value is the initial value
+     */
+    [name: string]: string;
+}
+
+export interface Values {
+    [name: string]: any;
+}
+enum ActionTypes { update, reset }
+export interface Action {
+    type: ActionTypes;
+    payload: Values;
+}
+
+function reducer(state: Values, action: Action) {
+    switch (action.type) {
+    case ActionTypes.update:
+        return {...state, ...action.payload};
+    case ActionTypes.reset:
+        return {...action.payload};
+    default:
+        throw new Error();
+    }
+}
+
+export function useFields(initialValues?: Fields) {
+    const [values, dispatch] = React.useReducer(reducer, initialValues || {});
+    dispatch;
+    return {values};
 }
--- test/useFields.test.ts
+++ test/useFields.test.ts
@@ -1,9 +1,17 @@
 import { renderHook } from 'react-hooks-testing-library'
-import { useFields } from "../src/useFields"
+import { useFields, Fields } from "../src/useFields"
 
 describe("useFields hook", () => {
+  test("should accept initial values as the only argument", () => {
+    const fields = {
+      name: "hello"
+    };
+    const { result } = renderHook<Fields, any>(() => useFields(fields));
+    expect(result.current.values).toEqual(fields);
+  });
+  
   test('should return values object, setValue function, resetValues function', () => {
-    const { result } = renderHook<any, any>(() => useFields());
+    const { result } = renderHook<Fields, any>(() => useFields());
   
     // values, setValue, resetValues
     expect(result.current.values).toBe("object");
