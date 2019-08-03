d9d0b9e423d2cf53f0d5053987f188a6960c5dc4
Jay Harris <jay@sourware.com>
2019-08-03 01:30:55 -0400

    added index file to src

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export {useForm} from "./useForms";
+export {useFields} from "./useFields";
+export {useErrors} from "./useErrors";
+export {useHandlers, useEventHandlers} from "./useHandlers";
+export {useSubmission} from "./useSubmission";
+export {useTouch} from "./useTouch";
+export {useValidator} from "./useValidator";
+// TODO: async await
