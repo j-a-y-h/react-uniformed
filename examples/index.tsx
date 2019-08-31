fe7cf0fb9d5cec3dc387ad50bb51a3aba0853775
Jay Harris <jay@sourware.com>
2019-08-31 15:58:25 -0400

    Makes basic the default example

--- examples/index.tsx
+++ examples/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './select';
+import Example from './basic';
 
 ReactDOM.render(<Example />, document.getElementById('root'));
