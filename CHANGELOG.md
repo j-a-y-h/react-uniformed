1cd950cfac18a35e3fb33a5b53221a438b22e520
Jay Harris <jay@sourware.com>
2019-09-01 10:12:20 -0400

    ResetValues no resets to empty string if no default value

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Fixed
+* [`useForm`, `useFields`] Resetting fields now sets the value to empty string if no default value
+
 ### Changed
 * [`useConstraint`] Update the number regex pattern
 
--- src/useFields.ts
+++ src/useFields.ts
@@ -1,4 +1,41 @@
-import { useResetableValues } from "./useResetableValues";
+import { useCallback } from "react";
+import { useResetableValues, UseResetableValuesHook, Values, MutableValues } from "./useResetableValues";
 
-// eslint-disable-next-line import/prefer-default-export
-export const useFields = useResetableValues;
+export type userSuppliedValue = string | string[] | boolean | number | undefined | null;
+export type Fields = Values<userSuppliedValue>;
+export type MutableFields = MutableValues<userSuppliedValue>;
+
+function getResetValue(currentValue: userSuppliedValue) {
+    switch (typeof currentValue) {
+    case "number":
+        return 0;
+    case "boolean":
+        return false;
+    case "object":
+        if (Array.isArray(currentValue)) {
+            return [];
+        } else {
+            return "";
+        }
+    case "string":
+    default:
+        return "";
+    }
+}
+
+export function useFields(initialValues?: Fields): UseResetableValuesHook<userSuppliedValue> {
+    const { resetValues: _, setValues, ...resetableValues } = useResetableValues(initialValues);
+    const resetValues = useCallback((): void => {
+        setValues((currentState) => {
+            const nonNullInitialValues: MutableFields = { ...initialValues } || {};
+            return Object.keys(currentState).reduce((newState, key) => {
+                if (!({}).hasOwnProperty.call(newState, key)) {
+                    newState[key] = getResetValue(currentState[key]);
+                }
+                return newState;
+            }, nonNullInitialValues);
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    return { ...resetableValues, setValues, resetValues };
+};
