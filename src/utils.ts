b47f100c061de0a4cb54cb47cf9edea24345bcfd
Jay Harris <jay@sourware.com>
2019-09-01 12:24:49 -0400

    Refactored

--- src/utils.ts
+++ src/utils.ts
@@ -6,41 +6,22 @@ export enum LoggingTypes {
     constraintError = "ConstraintError",
 }
 interface Logger {
-    (type: LoggingTypes, message: string): void;
+    (message?: string, ...optionalParams: string[]): void;
 }
 
-interface Log {
-    readonly [key: string]: Logger;
-}
-
-/* eslint-disable no-console */
 // eslint-disable-next-line import/prefer-default-export
-const log: Log = {
-    warning(type: LoggingTypes, message: string): void {
-        console.warn(`${projectName}: [${type}] ${message}`);
-    },
-    debug(type: LoggingTypes, message: string): void {
-        console.debug(`${projectName}: [${type}] ${message}`);
-    },
-    error(type: LoggingTypes, message: string): void {
-        console.error(`${projectName}: [${type}] ${message}`);
-    },
-};
-
 function assertion(condition: boolean, type: LoggingTypes, message: string, logger: Logger): void {
     if (!condition) {
-        logger(type, message);
+        /* eslint-disable no-console */
+        logger(`${projectName}: [${type}] ${message}`);
     }
 }
 
 export const assert = {
     error(condition: boolean, type: LoggingTypes, message: string): void {
-        assertion(condition, type, message, log.error);
+        assertion(condition, type, message, console.error);
     },
     warning(condition: boolean, type: LoggingTypes, message: string): void {
-        assertion(condition, type, message, log.warning);
-    },
-    debug(condition: boolean, type: LoggingTypes, message: string): void {
-        assertion(condition, type, message, log.debug);
+        assertion(condition, type, message, console.warn);
     },
 };
