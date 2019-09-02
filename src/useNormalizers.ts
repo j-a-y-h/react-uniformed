0628781de339e49bdbefa804c7bd47fbe9d45e73
Jay Harris <jay@sourware.com>
2019-09-02 19:43:53 -0400

    More documentation

--- src/useNormalizers.ts
+++ src/useNormalizers.ts
@@ -144,6 +144,37 @@ export function normalizeNestedObjects(): NormalizerHandler {
         }
     };
 }
+
+/**
+ * Creates a single normalizer function from the specified list of normalizers.
+ * note: order matters when passing normalizers. This means that the results or value
+ * of the first normalizer is passed to the next normalizer.
+ * @param {(NormalizerHandler | UseNormalizersOption)[]} normalizers if you
+ * pass a normalizer handler then it will apply to all fields. You can specify
+ * a specific list of fields by passing in
+ * @return {NormalizerHandler} returns a normalizer handler
+ * @example
+ *
+ * useNormalizers(
+ *    // apply to all fields
+ *    normalizeNestedObjects,
+ *    {
+ *      // apply to only fields ending in name (eg: firstName, lastName)
+ *      name: /name$/i,
+ *      normalizer: ({value}) => !value ? value : value.toUpperCase(),
+ *    },
+ *    {
+ *      // apply to only the date field
+ *      name: "date",
+ *      normalizer: ({value}) => !value ? value : value.replace(/-/g, "/"),
+ *    },
+ *    {
+ *      // apply to username or slug field
+ *      name: ["username", "slug"],
+ *      normalizer: ({value}) => !value ? value : value.toLowerCase(),
+ *    }
+ * )
+ */
 export function useNormalizers(
     ...normalizers: (NormalizerHandler | UseNormalizersOption)[]
 ): NormalizerHandler {
