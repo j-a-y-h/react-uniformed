a65a9b48481b91a67091b5ded639d2e1bacce919
Jay Harris <jay@sourware.com>
2019-09-02 19:48:51 -0400

    Misc udpates to useNormalizers

--- src/useNormalizers.ts
+++ src/useNormalizers.ts
@@ -1,62 +1,13 @@
 import { useMemo, useCallback } from "react";
 import { userSuppliedValue } from "./useFields";
 
-/*
-if a normalizer is set
-   then all calls to setValue, setValues are routed through the normalizer
-   the normalizers will have access to the input name, value, currentState, inputElement?
-   normalizers must return a value
-
-normalizer: (name, value, currentValues, element?) => userValue
-normalizer: (newValues, currentValues) => Object
-
-boundaries:
-    normalizing is for mapping a value to a property or
-
-NestedObjects
-   - read the current state and make precise updates
-   - syntax:
-    - Array: users[0], users[1]
-        - users: [String, String]
-    - Object: users[0][name], users[0][email]
-        - users: [{name: String, email: String}]
-
-ConvertToArray
-   - read the current state, create a unique id for the inputElement (fallback #id, then value)
-   - use a map to map updates then return the values of the map
-
-const normalizer = useNormalizer(nestedObjects, ConvertValuesToArray("value"), Convert(Boolean))
-const {} = useFields({}, opts: {normalizer});
-
-
-const normalizer = useNormalizer(nestedObjects);
-const {} = useForms({onSubmit, normalizer});
-
-
-const normalizer = useNormalizers(
-    nestedObjects,
-    {
-        names: [""],
-        normalizers: (v) => !v ? v : v.toLowerCase()
-    },
-    {
-        names: /name.+/,
-        normalizers: [nestedObjects, (v) => !v ? v : v.toUpperCase()]
-    }
-);
-
-normalizer("name[0][location]", "asfd", currentValues, element);
-
-const normalizer = useNormalizers(nestedObjects());
-const {} = useForms({onSubmit, normalizer});
-*/
 type NormalizeSetValue = Readonly<{
     name: string,
     value: any,
     currentValues: any,
     element?: any | null,
 }>;
-interface NormalizerHandler {
+export interface NormalizerHandler {
     (valuesUpdate: NormalizeSetValue): any;
 }
 export type UseNormalizersOption = Readonly<{
@@ -64,7 +15,7 @@ export type UseNormalizersOption = Readonly<{
     normalizer: NormalizerHandler;
 }>;
 
-function buildIndex({ currentValue, valueToSet, path, shadowCopy }: {
+function createNestedObject({ currentValue, valueToSet, path, shadowCopy }: {
     currentValue: any,
     valueToSet: userSuppliedValue,
     path: string[],
@@ -96,7 +47,7 @@ function buildIndex({ currentValue, valueToSet, path, shadowCopy }: {
             mergedValue = Object.assign({}, shadowCopy);
         }
     }
-    mergedValue[newIndex] = buildIndex({
+    mergedValue[newIndex] = createNestedObject({
         currentValue: mergedValue,
         valueToSet,
         path,
@@ -135,7 +86,7 @@ export function normalizeNestedObjects(): NormalizerHandler {
             return value;
         } else {
             const currentValueCopy = { [name]: currentValues[name] };
-            return buildIndex({
+            return createNestedObject({
                 currentValue: currentValueCopy,
                 valueToSet: value,
                 path: keys,
@@ -178,14 +129,12 @@ export function normalizeNestedObjects(): NormalizerHandler {
 export function useNormalizers(
     ...normalizers: (NormalizerHandler | UseNormalizersOption)[]
 ): NormalizerHandler {
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    const memoNormalizers = useMemo(() => normalizers, []);
     const normalize = useCallback(({ name, value, currentValues, element }: NormalizeSetValue) => {
         const nameMatches = (matcher: string | RegExp): boolean => {
             return matcher instanceof RegExp ? matcher.test(name) : matcher === name;
         };
         // pipe the value through the normalizers
-        return memoNormalizers.reduce((currentValue, normalizerObj) => {
+        return normalizers.reduce((currentValue, normalizerObj) => {
             let normalizer: NormalizerHandler | undefined;
             if (typeof normalizerObj === "function") {
                 // apply the normalizer to all
@@ -205,6 +154,7 @@ export function useNormalizers(
                 ? normalizer({ name, value: currentValue, currentValues, element })
                 : currentValue;
         }, value);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return normalize;
  }
