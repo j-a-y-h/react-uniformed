50147fd23c791fa209d59550c5241081e0821ddf
Jay Harris <jay@sourware.com>
2019-09-05 22:59:48 -0400

    added normalizer tests

---
+++ test/useNormalizers.test.ts
@@ -0,0 +1,7 @@
+import { renderHook, act } from 'react-hooks-testing-library'
+import { normalizeNestedObjects } from "../src/useNormalizers";
+
+describe("normalizeNestedObjects", () => {
+    it('can set errors', () => {
+    });
+});
