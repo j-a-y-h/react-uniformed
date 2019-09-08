5ef97943d76ae82c17bb7f516c77371c3c65812e
Jay Harris <jay@sourware.com>
2019-09-08 19:09:37 -0400

    Adds full coverage to useTouch

--- test/useTouch.test.ts
+++ test/useTouch.test.ts
@@ -1,2 +1,23 @@
+import { renderHook, act } from 'react-hooks-testing-library';
+import { useTouch } from '../src';
+
 describe("useTouch", () => {
+    it("API only supports values, setValue, resetValues, setValues, touchField", () => {
+        const { result } = renderHook(() => useTouch());
+        const { current } = result;
+        expect(typeof current.setTouch).toEqual("function");
+        expect(typeof current.resetTouches).toEqual("function");
+        expect(typeof current.touchField).toEqual("function");
+        expect(typeof current.setTouches).toEqual("function");
+        expect(current.touches).toMatchObject({});
+    });
+    it("supports setting a field to touch via a function with one parameter", () => {
+        const { result, waitForNextUpdate } = renderHook(() => useTouch());
+        act(() => {
+            result.current.touchField("name");
+        });
+        waitForNextUpdate().then(() => {
+            expect(result.current.touches.name).toEqual(true);
+        });
+    });
 });
