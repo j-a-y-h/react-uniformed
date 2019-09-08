f0e234c3aa493a405d48b9abb57c2f9d81d98079
Jay Harris <jay@sourware.com>
2019-09-08 19:25:47 -0400

    Adds error protection test

--- test/useSubmission.test.ts
+++ test/useSubmission.test.ts
@@ -5,6 +5,23 @@ describe("useSubmission", () => {
     it.todo("supports async validators");
     it.todo("supports async submit handlers");
     it.todo("doesn't allow invalid arguments");
+    it("only submits after the form is error free", () => {
+        const onSubmit = jest.fn(() => { });
+        const { result, waitForNextUpdate } = renderHook(() => useSubmission({
+            validator: () => ({ name: "this is an error" }),
+            onSubmit,
+        }));
+        act(() => {
+            result.current.submit();
+            result.current.submit();
+            result.current.submit();
+            result.current.submit();
+        });
+        waitForNextUpdate().then(() => {
+            expect(result.current.submitCount).toBe(0);
+            expect(onSubmit.mock.calls.length).toBe(0);
+        });
+    });
     it("calls preventDefault when submitting", () => {
         const { result, waitForNextUpdate } = renderHook(() => useSubmission({
             validator: () => ({}),
@@ -31,7 +48,7 @@ describe("useSubmission", () => {
             result.current.submit();
         });
         waitForNextUpdate().then(() => {
-            expect(result.current.submitCount).toEqual(4);
+            expect(result.current.submitCount).toBe(4);
         });
     });
 });
