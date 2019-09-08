065fe0200a76e8b429efcee33fc307aae0adbfe7
Jay Harris <jay@sourware.com>
2019-09-08 19:21:49 -0400

    Adds basic test coverage for useSubmission

--- test/useSubmission.test.ts
+++ test/useSubmission.test.ts
@@ -1,2 +1,37 @@
+import { renderHook, act } from 'react-hooks-testing-library';
+import { useSubmission } from '../src';
+
 describe("useSubmission", () => {
+    it.todo("supports async validators");
+    it.todo("supports async submit handlers");
+    it.todo("doesn't allow invalid arguments");
+    it("calls preventDefault when submitting", () => {
+        const { result, waitForNextUpdate } = renderHook(() => useSubmission({
+            validator: () => ({}),
+            onSubmit: () => { },
+        }));
+        const preventDefault = jest.fn(() => { });
+        act(() => {
+            // @ts-ignore
+            result.current.submit({ preventDefault });
+        });
+        waitForNextUpdate().then(() => {
+            expect(preventDefault.mock.calls.length).toBe(1);
+        });
+    });
+    it("supports a submission count", () => {
+        const { result, waitForNextUpdate } = renderHook(() => useSubmission({
+            validator: () => ({}),
+            onSubmit: () => { },
+        }));
+        act(() => {
+            result.current.submit();
+            result.current.submit();
+            result.current.submit();
+            result.current.submit();
+        });
+        waitForNextUpdate().then(() => {
+            expect(result.current.submitCount).toEqual(4);
+        });
+    });
 });
