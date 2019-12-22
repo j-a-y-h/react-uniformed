fb8b3052debef45e52029cfa914e5b27500c4450
Jay Harris <jay@sourware.com>
2019-12-22 13:32:03 -0500

    Add more test for useFunctionUtils

--- test/useFunctionUtils.test.ts
+++ test/useFunctionUtils.test.ts
@@ -1,8 +1,25 @@
 import { renderHook, act } from '@testing-library/react-hooks';
-import { useInvoking } from '../src/useFunctionUtils';
+import { useInvoking, useInvokeCount } from '../src/useFunctionUtils';
 
 describe("useInvokeCount", () => {
-    it("", () => { });
+    it("counts the number of times a function is invoked", () => {
+        const { result } = renderHook(() => useInvokeCount(() => { }));
+        act(() => {
+            result.current[0]();
+            result.current[0]();
+            result.current[0]();
+        });
+        expect(result.current[1]).toEqual(3);
+    });
+    it("supports async functions", () => {
+        const { result } = renderHook(() => useInvokeCount(async () => { }));
+        act(() => {
+            result.current[0]();
+            result.current[0]();
+            result.current[0]();
+        });
+        expect(result.current[1]).toEqual(3);
+    });
 });
 describe("useInvoking", () => {
     it("supports sync function", async () => {
@@ -17,13 +34,27 @@ describe("useInvoking", () => {
         }).catch(() => {});
         expect(result.current[1]).toEqual(false);
     })
-    it("supports async function", async () => {
+    it("determines if a function is being invoked", async () => {
         const { result, waitForNextUpdate } = renderHook(() => useInvoking(async () => { }));
         act(() => {
             result.current[0]();
         });
         expect(result.current[1]).toEqual(true);
         await waitForNextUpdate();
+        expect(result.current[1]).toEqual(false);
+    });
+    it("turns state to false for rejected async functions", async () => {
+        const { result, waitForNextUpdate } = renderHook(() => useInvoking(async () => {
+            return Promise.reject();
+         }));
+        act(() => {
+            result.current[0]().catch(() => {});
+        });
+        expect(result.current[1]).toEqual(true);
+        // @ts-ignore
+        await waitForNextUpdate({
+            timeout: 100,
+        });
         expect(result.current[1]).toEqual(false);
     });
 });
