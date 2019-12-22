fd83b6c0c5786c4c68299869e016e741494fa4b7
Jay Harris <jay@sourware.com>
2019-12-22 13:36:08 -0500

    Removed log statements

--- src/useSubmission.ts
+++ src/useSubmission.ts
@@ -1,7 +1,6 @@
 import { useCallback, SyntheticEvent } from 'react';
 import { hasValue } from './useGenericValues';
 import { Errors } from './useErrors';
-import { assert, LoggingTypes } from './utils';
 import { useInvokeCount, useInvoking } from './useFunctionUtils';
 
 export interface SubmissionHandler {
@@ -23,11 +22,6 @@ export interface UseSubmissionHook {
 
 // async handlers should return promises
 export function useSubmission({ validator, onSubmit }: UseSubmissionProps): UseSubmissionHook {
-  assert.error(
-    typeof validator === 'function' && typeof onSubmit === 'function',
-    LoggingTypes.typeError,
-    `(expected: function, function, received: ${typeof validator}, ${typeof onSubmit}) ${useSubmission.name} expects the properties named validator and onSubmit to be functions.`,
-  );
   // track submission count
   const [wrappedOnSubmit, submitCount] = useInvokeCount(onSubmit);
   const rawSubmissionHandler = useCallback(async (event?: SyntheticEvent): Promise<void> => {
