92a004d1af654ca762ffca3ef4fe3e9ca33a8b2e
Jay Harris <jay@sourware.com>
2019-12-22 12:55:26 -0500

    Add docs to useFunctionUtils

--- src/useFunctionUtils.ts
+++ src/useFunctionUtils.ts
@@ -1,5 +1,11 @@
 import { useState, useCallback } from 'react';
 
+/**
+ * Counts the number of times the specified function is invoked.
+ * @param fnc the specified function
+ * @return {[function, number]} an array where the first index is a function and
+ * the second argument is the number of times the function was called.
+ */
 export function useInvokeCount<T, K>(fnc: (...args: T[]) => K): [(...args: T[]) => K, number] {
   const [count, setCount] = useState(0);
   const wrappedFunction = useCallback((...args: T[]) => {
@@ -9,6 +15,13 @@ export function useInvokeCount<T, K>(fnc: (...args: T[]) => K): [(...args: T[]) 
   return [wrappedFunction, count];
 }
 
+/**
+ * Determines if the specified function is being called. This function
+ * is only useful for async functions.
+ * @param fnc the specified function
+ * @return {[function, boolean]} an array where the first index is a function and
+ * the second argument is the state of the invocation for the function.
+ */
 export function useInvoking<T, K>(
   fnc: (...args: T[]) => K | Promise<K>,
 ): [(...args: T[]) => K | Promise<K>, boolean] {
