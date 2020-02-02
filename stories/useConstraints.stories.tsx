41ed4f958cc5fc1b0b7dafaf19188485a9febb43
Jay Harris Jr <jay@sourware.com>
2020-02-02 11:02:16 -0500

    Chore/storybook (#18)
    * Adds a storybook directory
    
    * configured stories
    
    * Added a stories example
    
    * Moved storybook config to root directory
    
    * Renamed storybook directory to stories
    
    * Added console storybook addon
    
    * Added theme configuration
    
    * Updates lock file
    
    * updated npmignore

---
+++ stories/useConstraints.stories.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { useForm, useSettersAsEventHandler } from "../src";
+
+export default {
+    title: "useConstraints"
+}
+
+export function Form() {
+  const { setValue, validateByName, values, errors, submit } = useForm({
+    constraints: {
+      name: { required: true, minLength: 1, maxLength: 55 },
+      email: { required: true, type: "email" },
+      phone: { pattern: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/ },
+      website: { type: "url" },
+    },
+    onSubmit: data => alert(JSON.stringify(data)),
+  });
+  const handleChange = useSettersAsEventHandler(setValue);
+  const handleBlur = useSettersAsEventHandler(validateByName);
+  console.log(JSON.stringify(errors, null, 2));
+  return (
+    <form onSubmit={submit}>
+      <div>
+        <label>Name </label>
+        <input
+          type="text"
+          name="name"
+          value={values.name}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Email</label>
+        <input
+          type="text"
+          name="email"
+          value={values.email}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Mobile number</label>
+        <input
+          type="tel"
+          name="phone"
+          value={values.phone}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Website</label>
+        <input
+          type="text"
+          name="website"
+          value={values.website}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
+      </div>
+      <input type="submit" />
+    </form>
+  );
+}
