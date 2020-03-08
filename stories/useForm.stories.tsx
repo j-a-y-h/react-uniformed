bffff2aed7619ee0f1d100b19e9be5784529adc6
Jay Harris <jay@sourware.com>
2020-03-08 12:17:28 -0400

    Adds a story for useForm

---
+++ stories/useForm.stories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useForm, useSettersAsEventHandler } from "../src";
+
+export default {
+    title: "useForm"
+}
+
+export function Basic() {
+  const { setValue, validateByName, values, submit } = useForm({
+    onSubmit: data => alert(JSON.stringify(data)),
+  });
+  const handleChange = useSettersAsEventHandler(setValue, validateByName);
+  console.log(JSON.stringify(values, null, 2));
+  return (
+    <form onSubmit={submit}>
+      <div>
+        <label>Name </label>
+        <input
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Email</label>
+        <input
+          type="text"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+      </div>
+      <input type="submit" />
+    </form>
+  );
+}
