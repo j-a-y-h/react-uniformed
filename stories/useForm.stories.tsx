e45fc7fa338ed8753d2b2bd67c790b802dfeb8a5
Jay Harris <jay@sourware.com>
2020-03-08 12:21:30 -0400

    Added an example that fails during submission

--- stories/useForm.stories.tsx
+++ stories/useForm.stories.tsx
@@ -35,3 +35,37 @@ export function Basic() {
     </form>
   );
 }
+
+export function SubmissionFails() {
+    const { setValue, validateByName, values, submit } = useForm({
+        onSubmit(data) {
+            throw "Unexpected error occured";
+            alert(JSON.stringify(data));
+        },
+    });
+    const handleChange = useSettersAsEventHandler(setValue, validateByName);
+    console.log(JSON.stringify(values, null, 2));
+    return (
+      <form onSubmit={submit}>
+        <div>
+          <label>Name </label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+        </div>
+        <input type="submit" />
+      </form>
+    );
+  }
