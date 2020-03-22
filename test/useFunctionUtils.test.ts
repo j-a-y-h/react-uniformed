b143decb76914a054f8e633c65f3cdea27801034
Jay Harris <jay@sourware.com>
2020-03-22 15:38:44 -0400

    Fix function utils tests

--- test/useFunctionUtils.test.ts
+++ test/useFunctionUtils.test.ts
@@ -1,60 +1,58 @@
 import { renderHook, act } from '@testing-library/react-hooks';
-import { useInvoking, useInvokeCount } from '../src/useFunctionUtils';
+import { useFunctionStats } from '../src/useFunctionStats';
 
-describe("useInvokeCount", () => {
+describe("useFunctionStats", () => {
     it("counts the number of times a function is invoked", () => {
-        const { result } = renderHook(() => useInvokeCount(() => { }));
+        const { result } = renderHook(() => useFunctionStats(() => { }));
         act(() => {
-            result.current[0]();
-            result.current[0]();
-            result.current[0]();
+            result.current.fnc();
+            result.current.fnc();
+            result.current.fnc();
         });
-        expect(result.current[1]).toEqual(3);
+        expect(result.current.invokeCount).toEqual(3);
     });
     it("supports async functions", () => {
-        const { result } = renderHook(() => useInvokeCount(async () => { }));
+        const { result } = renderHook(() => useFunctionStats(async () => { }));
         act(() => {
-            result.current[0]();
-            result.current[0]();
-            result.current[0]();
+            result.current.fnc();
+            result.current.fnc();
+            result.current.fnc();
         });
-        expect(result.current[1]).toEqual(3);
+        expect(result.current.invokeCount).toEqual(3);
     });
-});
-describe("useInvoking", () => {
     it("supports sync function", async () => {
-        const { result, waitForNextUpdate } = renderHook(() => useInvoking(() => { }));
+        const { result, waitForNextUpdate } = renderHook(() => useFunctionStats(() => { }));
         act(() => {
-            result.current[0]();
+            result.current.fnc();
         });
-        expect(result.current[1]).toEqual(false);
+        expect(result.current.isRunning).toEqual(false);
         // @ts-ignore
         await waitForNextUpdate({
             timeout: 100,
         }).catch(() => {});
-        expect(result.current[1]).toEqual(false);
+        expect(result.current.isRunning).toEqual(false);
     })
     it("determines if a function is being invoked", async () => {
-        const { result, waitForNextUpdate } = renderHook(() => useInvoking(async () => { }));
+        const { result, waitForNextUpdate } = renderHook(() => useFunctionStats(async () => { }));
         act(() => {
-            result.current[0]();
+            result.current.fnc();
         });
-        expect(result.current[1]).toEqual(true);
+        expect(result.current.isRunning).toEqual(true);
         await waitForNextUpdate();
-        expect(result.current[1]).toEqual(false);
+        expect(result.current.isRunning).toEqual(false);
     });
     it("turns state to false for rejected async functions", async () => {
-        const { result, waitForNextUpdate } = renderHook(() => useInvoking(async () => {
+        const { result, waitForNextUpdate } = renderHook(() => useFunctionStats(async () => {
             return Promise.reject();
          }));
         act(() => {
-            result.current[0]().catch(() => {});
+            result.current.fnc().catch(() => {});
         });
-        expect(result.current[1]).toEqual(true);
+        expect(result.current.isRunning).toEqual(true);
         // @ts-ignore
         await waitForNextUpdate({
             timeout: 100,
         });
-        expect(result.current[1]).toEqual(false);
+        expect(result.current.isRunning).toEqual(false);
     });
 });
