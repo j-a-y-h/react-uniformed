35426a7d59baf12b301df7af91fb9e671800314b
Jay Harris Jr <jay@sourware.com>
2020-04-04 14:30:48 -0400

    Feature/add dirty field (#26)
    * adds isDirty field
    
    * Updates docs
    
    * Adds test for isDirty field

--- test/useTouch.test.ts
+++ test/useTouch.test.ts
@@ -2,7 +2,7 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import { useTouch } from '../src';
 
 describe("useTouch", () => {
-    it("API only supports values, setValue, resetValues, setValues, touchField", () => {
+    it("API only supports values, setValue, resetValues, setValues, touchField, isDirty", () => {
         const { result } = renderHook(() => useTouch());
         const { current } = result;
         expect(typeof current.setTouch).toEqual("function");
@@ -10,6 +10,27 @@ describe("useTouch", () => {
         expect(typeof current.touchField).toEqual("function");
         expect(typeof current.setTouches).toEqual("function");
         expect(current.touches).toMatchObject({});
+        expect(current.isDirty).toEqual(false);
+    });
+    it("sets isDirty to true if any value is touched", () => {
+        const { result } = renderHook(() => useTouch());
+        act(() => {
+            result.current.touchField("name");
+        });
+        expect(result.current.isDirty).toEqual(true);
+    });
+    it("sets isDirty to false if touches are reset", () => {
+        const { result } = renderHook(() => useTouch());
+
+        expect(result.current.isDirty).toEqual(false);
+        act(() => {
+            result.current.touchField("name");
+        });
+        expect(result.current.isDirty).toEqual(true);
+        act(() => {
+            result.current.resetTouches();
+        });
+        expect(result.current.isDirty).toEqual(false);
     });
     it("supports setting a field to touch via a function with one parameter", async () => {
         // @ts-ignore
