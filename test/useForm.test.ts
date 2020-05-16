2f1351f7306a6bcd48e21b713e7f941dcfcc77e5
Jay Harris <jay@sourware.com>
2020-05-16 02:59:13 -0400

    Started initial testing of useForm

--- test/useForm.test.ts
+++ test/useForm.test.ts
@@ -1,3 +1,33 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useForm } from "../src";
+
+const ERROR = "__required__";
+const SUCCESS = "";
+
 describe("useForm", () => {
-    it("", () => { });
+    describe("with constraints", () => {
+        it("supports email types", async () => {
+            const { result, waitForNextUpdate } = renderHook(() => useForm({
+                onSubmit() { },
+                constraints: {
+                    email: { type: "email" },
+                }
+            }));
+            const subtest = async (value, shouldFail?) => {
+                act(() => {
+                    result.current.validateByName('email', value)
+                });
+                await waitForNextUpdate();
+                let exp = expect(result.current.errors.email);
+                if (shouldFail) {
+                    // @ts-expect-error
+                    exp = exp.not;
+                }
+                exp.toEqual(SUCCESS);
+            };
+            await subtest("example.com", true);
+            await subtest("e@example.com");
+            await subtest("john.doe@example.uk.co");
+        });
+    });
 });
