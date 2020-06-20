604a86f9f981953535955dad3623ccf80bec0e06
Jay Harris <jay@sourware.com>
2020-06-20 03:06:54 -0400

    Formats stories using prettier

--- stories/useSubmission.stories.tsx
+++ stories/useSubmission.stories.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { useSubmission } from "../src";
+import React, { useState } from 'react';
+import { useSubmission } from '../src';
 
 export default {
-  title: "useSubmission"
-}
+  title: 'useSubmission',
+};
 
 const sleep = (duration: number) => {
   return new Promise<void>((res) => {
@@ -11,36 +11,30 @@ const sleep = (duration: number) => {
       res();
     }, duration * 1000);
   });
-}
-
-
+};
 
 export function Form() {
   const [hasErrors, setHasErrors] = useState(false);
   const { isSubmitting, submitCount, submit } = useSubmission({
     onSubmit: () => {
-      console.log("called");
+      console.log('called');
       return sleep(1);
     },
     disabled: hasErrors,
     validator() {
       const isValid = Boolean(Math.floor(Math.random() * 2));
       setHasErrors(isValid);
-    }
+    },
   });
   return (
     <form onSubmit={submit}>
-      {isSubmitting && (<p>Is Submitting...</p>)}
-      {hasErrors && (<p>There is a random error.</p>)}
+      {isSubmitting && <p>Is Submitting...</p>}
+      {hasErrors && <p>There is a random error.</p>}
       <div>
         <label>Name </label>
-        <input
-          type="text"
-          name="name"
-          value="John"
-        />
+        <input type='text' name='name' value='John' />
       </div>
-      <input disabled={isSubmitting} type="submit" />
+      <input disabled={isSubmitting} type='submit' />
       <div>
         <label>Submit count: {submitCount}</label>
       </div>
@@ -53,29 +47,30 @@ export function FormWithRandomValidation() {
   const [clicks, setClicks] = useState(0);
   const { submitCount, isSubmitting, submit } = useSubmission({
     onSubmit: () => {
-      console.log("called");
+      console.log('called');
       return sleep(0.5);
     },
     disabled: hasErrors,
     validator() {
       const isValid = Boolean(Math.floor(Math.random() * 2));
       setHasErrors(isValid);
-    }
+    },
   });
   return (
-    <form onSubmit={(e) => { setClicks(i => i + 1); submit(e); }}>
+    <form
+      onSubmit={(e) => {
+        setClicks((i) => i + 1);
+        submit(e);
+      }}>
       <div>
         <label>Name </label>
-        <input
-          type="text"
-          name="name"
-          value="John"
-          disabled={hasErrors}
-        />
+        <input type='text' name='name' value='John' disabled={hasErrors} />
       </div>
-      <input disabled={isSubmitting} type="submit" />
+      <input disabled={isSubmitting} type='submit' />
       <div>
-        <label>Submit count / submit attempts: {submitCount}/{clicks}</label>
+        <label>
+          Submit count / submit attempts: {submitCount}/{clicks}
+        </label>
         <br />
         <label>Has Errors: {JSON.stringify(hasErrors)}</label>
       </div>
@@ -83,37 +78,37 @@ export function FormWithRandomValidation() {
   );
 }
 
-
 export function FormWithAsyncValidator() {
   const [hasErrors, setHasErrors] = useState(false);
   const [clicks, setClicks] = useState(0);
   const { submitCount, isSubmitting, submit } = useSubmission({
     onSubmit: () => {
-      console.log("called");
+      console.log('called');
     },
     disabled: hasErrors,
     validator() {
-      console.log("validating");
+      console.log('validating');
       return sleep(5).then(() => {
-        console.log("done validating");
+        console.log('done validating');
         setHasErrors((i) => !i);
       });
-    }
+    },
   });
   return (
-    <form onSubmit={(e) => { setClicks(i => i + 1); submit(e); }}>
+    <form
+      onSubmit={(e) => {
+        setClicks((i) => i + 1);
+        submit(e);
+      }}>
       <div>
         <label>Name </label>
-        <input
-          type="text"
-          name="name"
-          value="John"
-          disabled={hasErrors}
-        />
+        <input type='text' name='name' value='John' disabled={hasErrors} />
       </div>
-      <input disabled={isSubmitting} type="submit" />
+      <input disabled={isSubmitting} type='submit' />
       <div>
-        <label>Submit count / submit attempts: {submitCount}/{clicks}</label>
+        <label>
+          Submit count / submit attempts: {submitCount}/{clicks}
+        </label>
         <br />
         <label>Has Errors: {JSON.stringify(hasErrors)}</label>
       </div>
--- stories/zPerformance.stories.tsx
+++ stories/zPerformance.stories.tsx
@@ -1,50 +1,50 @@
-import React from "react";
-import { useForm, useSettersAsRefEventHandler } from "../src";
+import React from 'react';
+import { useForm, useSettersAsRefEventHandler } from '../src';
 
 export default {
-  title: "Performance"
-}
+  title: 'Performance',
+};
 
 const createArray = (length) => Array.from({ length }, (_, k) => k + 1);
 const createKeys = (prefix, length = 5000) => createArray(length).map((_, i) => `${prefix}${i}`);
 
 const emailKeys = {
-  100: createKeys("email", 100),
-  1000: createKeys("email", 1000),
+  100: createKeys('email', 100),
+  1000: createKeys('email', 1000),
 };
 
-function Form({values, errors, submit, changeRef, emails}) {
+function Form({ values, errors, submit, changeRef, emails }) {
   return (
     <div>
       <h1>Performance Test (Input Count: {emails.length})</h1>
-      <br/>
+      <br />
       <table>
-      <tr>
-        <td style={{verticalAlign: "top", width: "25%"}}>
+        <tr>
+          <td style={{ verticalAlign: 'top', width: '25%' }}>
             <form onSubmit={submit}>
-            <button type="submit">Submit</button>
-            <br />
-            {emails.map(key => (
-              <React.Fragment key={key}>
-                <label>{key}:</label>
-                <input name={key} ref={changeRef} />
-                <span style={{color: 'red'}}>{errors[key] && <div>{errors[key]}</div>}</span>
-                <br />
-              </React.Fragment>
-            ))}
-            <input name="username" ref={changeRef} />
-            {errors.username && <div>{errors.username}</div>}
-          </form>
-        </td>
-        <td style={{verticalAlign: "top", width: "25%"}}>
-          <p>Errors</p>
-          <pre>{JSON.stringify(errors, null, 2)}</pre>
-        </td>
-        <td style={{verticalAlign: "top", width: "25%"}}>
-          <p>Values</p>
-          <pre>{JSON.stringify(values, null, 2)}</pre>
-        </td>
-      </tr>
+              <button type='submit'>Submit</button>
+              <br />
+              {emails.map((key) => (
+                <React.Fragment key={key}>
+                  <label>{key}:</label>
+                  <input name={key} ref={changeRef} />
+                  <span style={{ color: 'red' }}>{errors[key] && <div>{errors[key]}</div>}</span>
+                  <br />
+                </React.Fragment>
+              ))}
+              <input name='username' ref={changeRef} />
+              {errors.username && <div>{errors.username}</div>}
+            </form>
+          </td>
+          <td style={{ verticalAlign: 'top', width: '25%' }}>
+            <p>Errors</p>
+            <pre>{JSON.stringify(errors, null, 2)}</pre>
+          </td>
+          <td style={{ verticalAlign: 'top', width: '25%' }}>
+            <p>Values</p>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </td>
+        </tr>
       </table>
     </div>
   );
@@ -54,26 +54,42 @@ export function BigFormWithValidation() {
   const { setValue, submit, errors, values, validateByName } = useForm({
     constraints: (values) => {
       return Object.keys(values).reduce((cur, key) => {
-        return {...cur, [key]: {type: ['email', 'Please enter a valid email'], required: true}}
-      }, {})
+        return { ...cur, [key]: { type: ['email', 'Please enter a valid email'], required: true } };
+      }, {});
     },
-    onSubmit: data => alert(JSON.stringify(data)),
+    onSubmit: (data) => alert(JSON.stringify(data)),
   });
   const handleChange = useSettersAsRefEventHandler<HTMLInputElement>(setValue, validateByName);
 
-  return <Form emails={emailKeys[100]} changeRef={handleChange} values={values} errors={errors} submit={submit} />
+  return (
+    <Form
+      emails={emailKeys[100]}
+      changeRef={handleChange}
+      values={values}
+      errors={errors}
+      submit={submit}
+    />
+  );
 }
 
 export function BigFormWith1000Inputs() {
   const { setValue, submit, errors, values, validateByName } = useForm({
     constraints: (values) => {
       return Object.keys(values).reduce((cur, key) => {
-        return {...cur, [key]: {type: ['email', 'Please enter a valid email'], required: true}}
-      }, {})
+        return { ...cur, [key]: { type: ['email', 'Please enter a valid email'], required: true } };
+      }, {});
     },
-    onSubmit: data => alert(JSON.stringify(data)),
+    onSubmit: (data) => alert(JSON.stringify(data)),
   });
   const handleChange = useSettersAsRefEventHandler<HTMLInputElement>(setValue, validateByName);
 
-  return <Form emails={emailKeys[1000]} changeRef={handleChange} values={values} errors={errors} submit={submit} />
+  return (
+    <Form
+      emails={emailKeys[1000]}
+      changeRef={handleChange}
+      values={values}
+      errors={errors}
+      submit={submit}
+    />
+  );
 }
