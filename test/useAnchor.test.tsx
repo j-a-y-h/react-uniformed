3fdf999cfd1ecb18a9e1b5a756b9787491722cb9
Jay Harris <jay@sourware.com>
2020-07-12 21:50:20 -0400

    Improves test

--- test/useAnchor.test.tsx
+++ test/useAnchor.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { useAnchor } from '../src';
 
 describe('useAnchor', () => {
-  it('sets change event handlers on form elements', () => {
+  it('sets change event handlers on form elements', async () => {
     const props = {
       handleChange: jest.fn(),
     };
@@ -14,11 +14,13 @@ describe('useAnchor', () => {
       <form ref={result.current.anchor}>
         <div>
           <label>Name </label>
-          <input type='text' name='name' />
+          <input type='text' name='name' title='name' />
         </div>
       </form>,
     );
+    const name = await mount.findByTitle('name');
 
+    fireEvent(name, new Event('change'));
     expect(props.handleChange).toBeCalledTimes(1);
   });
 });
