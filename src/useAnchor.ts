02f63cb730067d518616d7299f9d18ae9659a241
Jay Harris <jay@sourware.com>
2020-07-12 22:46:20 -0400

    Extracts logic to mount inputs

--- src/useAnchor.ts
+++ src/useAnchor.ts
@@ -7,6 +7,7 @@ type Props = Readonly<{
   values?: Fields;
   handleChange?: ReactOrNativeEventListener;
   handleBlur?: ReactOrNativeEventListener;
+  handleSubmit?: ReactOrNativeEventListener;
 }>;
 
 type ValidFormElements = HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement;
@@ -15,36 +16,43 @@ interface UseAnchor {
   anchor: Ref<HTMLFormElement>;
 }
 
+function mountInputs(
+  { elements }: HTMLFormElement,
+  handleChange?: ReactOrNativeEventListener,
+  handleBlur?: ReactOrNativeEventListener,
+): void {
+  // filter for input, select, and textarea elements only
+  const validElements = Array.from(elements).filter((element) => {
+    return (
+      element instanceof HTMLInputElement ||
+      element instanceof HTMLSelectElement ||
+      element instanceof HTMLTextAreaElement
+    );
+  });
+  // add event handlers
+  validElements.forEach((input) => {
+    const handlers: [string, ReactOrNativeEventListener | undefined][] = [
+      ['change', handleChange],
+      ['blur', handleBlur],
+    ];
+    // add each event handler
+    handlers.forEach(([event, eventHandler]) => {
+      if (eventHandler) {
+        mountEventHandler({
+          input: (input as unknown) as ValidFormElements,
+          event,
+          eventHandler,
+        });
+      }
+    });
+  });
+}
+
 export function useAnchor({ handleChange, handleBlur }: Props): UseAnchor {
   const anchor = useCallback(
     (form: HTMLFormElement | null): void => {
       if (form) {
-        const { elements } = form;
-        // filter for input, select, and textarea elements only
-        const validElements = Array.from(elements).filter((element) => {
-          return (
-            element instanceof HTMLInputElement ||
-            element instanceof HTMLSelectElement ||
-            element instanceof HTMLTextAreaElement
-          );
-        });
-        // add event handlers
-        validElements.forEach((input) => {
-          const handlers: [string, ReactOrNativeEventListener | undefined][] = [
-            ['change', handleChange],
-            ['blur', handleBlur],
-          ];
-          // add each event handler
-          handlers.forEach(([event, eventHandler]) => {
-            if (eventHandler) {
-              mountEventHandler({
-                input: (input as unknown) as ValidFormElements,
-                event,
-                eventHandler,
-              });
-            }
-          });
-        });
+        mountInputs(form, handleChange, handleBlur);
       }
     },
     [handleChange, handleBlur],
