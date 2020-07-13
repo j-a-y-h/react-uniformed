f3d1b3c7b28f431d0780fcaf69582b7f14df86b7
Jay Harris <jay@sourware.com>
2020-07-13 19:02:20 -0400

    Attempts to deregister events on unmount

--- src/useRefEventHandlers.ts
+++ src/useRefEventHandlers.ts
@@ -1,4 +1,4 @@
-import { Ref, useCallback } from 'react';
+import { Ref, useCallback, useRef } from 'react';
 import { ReactOrNativeEventListener } from './useSettersAsEventHandler';
 import { useHandlers } from './useHandlers';
 
@@ -7,20 +7,30 @@ type Props = Readonly<{
   handlers: ReactOrNativeEventListener[];
 }>;
 
+interface LastRef<T> {
+  input: T | null;
+  eventHandler?: ReactOrNativeEventListener;
+  event: string;
+}
+
 export function useRefEventHandlers<T extends HTMLElement = HTMLElement>({
   event,
   handlers,
 }: Props): Ref<T> {
+  const lastRef = useRef<LastRef<T>>();
   const eventHandler = useHandlers(...handlers);
   const ref = useCallback(
     (input: T | null): void => {
       // note: React will call input with null when the component is unmounting
       if (input) {
-        // TODO: solve potential memory leak, in the else block removeEventListener,
-        // and when this function
-        // is called with new eventHandler
+        // adds event listener on mount
         input.addEventListener(event, eventHandler);
+      } else if (lastRef.current?.eventHandler) {
+        // removes the event listener
+        const { current } = lastRef;
+        current?.input?.removeEventListener(current.event, lastRef.current.eventHandler);
       }
+      lastRef.current = { event, eventHandler, input };
     },
     [event, eventHandler],
   );
