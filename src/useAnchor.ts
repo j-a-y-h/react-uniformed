b0a44ab8b8a26249bb42f79256afb47f7d51f483
Jay Harris <jay@sourware.com>
2020-07-12 22:09:58 -0400

    Passes first handleChange test

--- src/useAnchor.ts
+++ src/useAnchor.ts
@@ -1,6 +1,7 @@
 import { Ref, useCallback } from 'react';
 import { Fields } from './useFields';
 import { ReactOrNativeEventListener } from './useSettersAsEventHandler';
+import { mountEventHandler } from './utils';
 
 type Props = Readonly<{
   values?: Fields;
@@ -12,7 +13,33 @@ interface UseAnchor {
   anchor: Ref<HTMLFormElement>;
 }
 
-export function useAnchor({}: Props): UseAnchor {
-  const anchor = useCallback((form: HTMLFormElement | null): void => {}, []);
+export function useAnchor({ handleChange }: Props): UseAnchor {
+  const anchor = useCallback(
+    (form: HTMLFormElement | null): void => {
+      if (form) {
+        const { elements } = form;
+        // filter for input, select, and textarea elements only
+        const validElements = Array.from(elements).filter((element) => {
+          return (
+            element instanceof HTMLInputElement ||
+            element instanceof HTMLSelectElement ||
+            element instanceof HTMLTextAreaElement
+          );
+        });
+        // add event handlers
+        validElements.forEach((input) => {
+          if (handleChange) {
+            mountEventHandler({
+              // TODO: support of types here too
+              input: (input as unknown) as HTMLInputElement,
+              event: 'change',
+              eventHandler: handleChange,
+            });
+          }
+        });
+      }
+    },
+    [handleChange],
+  );
   return { anchor };
 }
