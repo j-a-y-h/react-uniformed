6b187c2286433e1382d2d4ad89d03864b5184649
Jay Harris <jay@sourware.com>
2020-07-13 21:19:14 -0400

    Replaces useFormAnchor with useRefEventHandlers

--- src/useFormRef/useFormRef.ts
+++ src/useFormRef/useFormRef.ts
@@ -1,8 +1,8 @@
 import { Ref } from 'react';
 import { ReactOrNativeEventListener } from '../useSettersAsEventHandler';
 import { useAnchorInputs } from './useFormInputsRef';
-import { useFormAnchor } from './useFormAnchor';
 import { useHandlers } from '../useHandlers';
+import { useRefEventHandlers } from '../useRefEventHandlers';
 
 type Props = Readonly<{
   handleChange?: ReactOrNativeEventListener;
@@ -24,8 +24,8 @@ export function useFormRef({
 }: Props): UseAnchor {
   const handleInputs = useAnchorInputs({ handleBlur, handleChange });
   // TODO: make a useRefEventHandler hook for this, formsRef, inputsRet and useSettersAsRefEventHandler
-  const handleFormSubmit = useFormAnchor({ handler: handleSubmit, type: 'submit' });
-  const handleFormReset = useFormAnchor({ handler: handleReset, type: 'reset' });
+  const handleFormSubmit = useRefEventHandlers({ handlers: [handleSubmit], event: 'submit' });
+  const handleFormReset = useRefEventHandlers({ handlers: [handleReset], event: 'reset' });
   const anchor = useHandlers(handleInputs, handleFormSubmit, handleFormReset);
   return { anchor };
 }
