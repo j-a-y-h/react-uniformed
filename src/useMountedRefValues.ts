1f4358a61ad79184269698a18a6afe86f152b351
Jay Harris <jay@sourware.com>
2020-07-13 20:15:34 -0400

    Passes almost all tests

--- src/useMountedRefValues.ts
+++ src/useMountedRefValues.ts
@@ -5,9 +5,27 @@ type Props = Readonly<{
   values: Fields;
 }>;
 
-export function useMountedRefValues<T extends HTMLElement = HTMLElement>({
+type ValueLikeElement =
+  | Pick<HTMLInputElement, 'value' | 'name'>
+  | Pick<HTMLSelectElement, 'value' | 'name'>
+  | Pick<HTMLTextAreaElement, 'value' | 'name'>;
+
+export function useMountedRefValues<T extends ValueLikeElement = HTMLInputElement>({
   values,
 }: Props): Ref<T> {
-  const ref = useCallback((input: T | null): void => {}, []);
+  // TODO: console warn if the node already has a value
+  const ref = useCallback(
+    (input: T | null): void => {
+      if (input) {
+        const value = values[input.name];
+        if (['string', 'number'].includes(typeof value)) {
+          // need to set the mounted values
+          // eslint-disable-next-line no-param-reassign
+          input.value = value;
+        }
+      }
+    },
+    [values],
+  );
   return ref;
 }
