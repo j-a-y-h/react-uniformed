fd3b3e46f03b03f18573d469d118d822b05187b2
Jay Harris <jay@sourware.com>
2020-07-14 21:53:16 -0400

    Adds test for multiple elements

--- test/useRefEventHandlers.test.tsx
+++ test/useRefEventHandlers.test.tsx
@@ -11,73 +11,124 @@ describe('useRefEventHandlers', () => {
       event,
     };
   }
-  it.each(['click', 'focus'])('sets %s event handlers using a ref', async (event) => {
-    const props = createMockHandlers(event);
-    const { result } = renderHook(() => useRefEventHandlers<HTMLButtonElement>(props));
+  describe('when used with single elements', () => {
+    it.each(['click', 'focus'])('sets %s event handlers using a ref', async (event) => {
+      const props = createMockHandlers(event);
+      const { result } = renderHook(() => useRefEventHandlers<HTMLButtonElement>(props));
 
-    const mount = render(
-      <button ref={result.current} title='name'>
-        Clicker
-      </button>,
-    );
-    const name = await mount.findByTitle('name');
-    props.handlers.forEach((handler) => {
-      expect(handler).toBeCalledTimes(0);
-    });
-    fireEvent(name, new Event(event));
-    props.handlers.forEach((handler) => {
-      expect(handler).toBeCalledTimes(1);
-    });
-  });
-  it('will not set event handlers if event handler is not specified', async () => {
-    const props = { event: 'click' };
-    const { result } = renderHook(() => useRefEventHandlers<HTMLButtonElement>(props));
-    const mockEventListener = {
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-    };
-    result.current((mockEventListener as unknown) as HTMLInputElement);
-    expect(mockEventListener.addEventListener).toBeCalledTimes(0);
-    expect(mockEventListener.removeEventListener).toBeCalledTimes(0);
-  });
-  it.each([['click'], ['blur']])('removes %s event handlers on unmount', async (event) => {
-    const props = createMockHandlers(event);
-    const props2 = createMockHandlers(event);
-    const { result, rerender } = renderHook(
-      (props) => {
-        return useRefEventHandlers<HTMLButtonElement>(props);
-      },
-      {
-        initialProps: props,
-      },
-    );
-
-    const Component = React.forwardRef((_, ref: React.Ref<HTMLButtonElement>) => (
-      <button ref={ref} title='name'>
-        Clicker
-      </button>
-    ));
-    const mount = render(<Component ref={result.current} />);
-    const trigger = async () => {
+      const mount = render(
+        <button ref={result.current} title='name'>
+          Clicker
+        </button>,
+      );
       const name = await mount.findByTitle('name');
+      props.handlers.forEach((handler) => {
+        expect(handler).toBeCalledTimes(0);
+      });
       fireEvent(name, new Event(event));
-    };
-    await trigger();
-    props.handlers.forEach((handler) => {
-      expect(handler).toBeCalledTimes(1);
+      props.handlers.forEach((handler) => {
+        expect(handler).toBeCalledTimes(1);
+      });
     });
-    props2.handlers.forEach((handler) => {
-      expect(handler).toBeCalledTimes(0);
+    it('will not set event handlers if event handler is not specified', async () => {
+      const props = { event: 'click' };
+      const { result } = renderHook(() => useRefEventHandlers<HTMLButtonElement>(props));
+      const mockEventListener = {
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+      };
+      result.current((mockEventListener as unknown) as HTMLInputElement);
+      expect(mockEventListener.addEventListener).toBeCalledTimes(0);
+      expect(mockEventListener.removeEventListener).toBeCalledTimes(0);
     });
+    it.each([['click'], ['blur']])('removes %s event handlers on unmount', async (event) => {
+      const props = createMockHandlers(event);
+      const props2 = createMockHandlers(event);
+      const { result, rerender } = renderHook(
+        (props) => {
+          return useRefEventHandlers<HTMLButtonElement>(props);
+        },
+        {
+          initialProps: props,
+        },
+      );
 
-    rerender(props2);
-    mount.rerender(<Component ref={result.current} />);
-    await trigger();
-    props.handlers.forEach((handler) => {
-      expect(handler).toBeCalledTimes(1);
+      const Component = React.forwardRef((_, ref: React.Ref<HTMLButtonElement>) => (
+        <button ref={ref} title='name'>
+          Clicker
+        </button>
+      ));
+      const mount = render(<Component ref={result.current} />);
+      const trigger = async () => {
+        const name = await mount.findByTitle('name');
+        fireEvent(name, new Event(event));
+      };
+      await trigger();
+      props.handlers.forEach((handler) => {
+        expect(handler).toBeCalledTimes(1);
+      });
+      props2.handlers.forEach((handler) => {
+        expect(handler).toBeCalledTimes(0);
+      });
+
+      rerender(props2);
+      mount.rerender(<Component ref={result.current} />);
+      await trigger();
+      props.handlers.forEach((handler) => {
+        expect(handler).toBeCalledTimes(1);
+      });
+      props2.handlers.forEach((handler) => {
+        expect(handler).toBeCalledTimes(1);
+      });
     });
-    props2.handlers.forEach((handler) => {
-      expect(handler).toBeCalledTimes(1);
+  });
+  describe('when used with multiple elements', () => {
+    it.each([['click'], ['blur']])('removes %s event handlers on unmount', async (event) => {
+      const props = createMockHandlers(event);
+      const props2 = createMockHandlers(event);
+      const { result, rerender } = renderHook(
+        (props) => {
+          return useRefEventHandlers<HTMLButtonElement>(props);
+        },
+        {
+          initialProps: props,
+        },
+      );
+
+      const Component = React.forwardRef((_, ref: React.Ref<HTMLButtonElement>) => (
+        <>
+          <button ref={ref} title='name'>
+            Clicker
+          </button>
+          <button ref={ref} title='name'>
+            Clicker
+          </button>
+        </>
+      ));
+      const mount = render(<Component ref={result.current} />);
+      const trigger = async () => {
+        const names = await mount.findAllByTitle('name');
+        names.forEach((name) => {
+          fireEvent(name, new Event(event));
+        });
+      };
+      await trigger();
+      props.handlers.forEach((handler) => {
+        expect(handler).toBeCalledTimes(1);
+      });
+      props2.handlers.forEach((handler) => {
+        expect(handler).toBeCalledTimes(0);
+      });
+
+      rerender(props2);
+      mount.rerender(<Component ref={result.current} />);
+      await trigger();
+      props.handlers.forEach((handler) => {
+        expect(handler).toBeCalledTimes(1);
+      });
+      props2.handlers.forEach((handler) => {
+        expect(handler).toBeCalledTimes(1);
+      });
     });
   });
 });
