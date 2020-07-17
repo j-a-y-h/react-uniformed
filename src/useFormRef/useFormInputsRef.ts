7540041fc61fb3bd73434f8838321e85117c2d46
Jay Harris <jay@sourware.com>
2020-07-17 02:58:29 -0400

    Code clean up

--- src/useFormRef/useFormInputsRef.ts
+++ src/useFormRef/useFormInputsRef.ts
@@ -1,4 +1,4 @@
-import { useCallback, RefCallback, createRef, useEffect, useRef } from 'react';
+import { useCallback, RefCallback, useEffect, useRef } from 'react';
 import { ReactOrNativeEventListener } from '../useSettersAsEventHandler';
 import { useRefEventHandlers } from '../useRefEventHandlers';
 import { useHandlers } from '../useHandlers';
@@ -20,35 +20,42 @@ export function useFormInputsRef({
   const changeRef = useRefEventHandlers({ handlers: handleChange, event: 'change' });
   const blurRef = useRefEventHandlers({ handlers: handleBlur, event: 'blur' });
   const ref = useHandlers(changeRef, blurRef);
-  const testRef = useRef<HTMLFormElement | null>();
-  const update = useCallback((): void => {
-    const form = testRef.current;
-    // mounts
-    if (form) {
-      // filter for input, select, and textarea elements only
-      const supportedElements = Array.from(form.elements).filter<SupportedFormElements>(
-        (element): element is SupportedFormElements => {
-          return (
-            // inputs must be of certain type too
-            (element instanceof HTMLInputElement && !UNSUPPORTED_INPUT_TYPES.has(element.type)) ||
-            element instanceof HTMLSelectElement ||
-            element instanceof HTMLTextAreaElement
-          );
-        },
-      );
-      // add event handlers
-      supportedElements.forEach(ref);
-    } else {
-      // unmount
-      ref(null);
-    }
-  }, [ref, testRef]);
+  const formRef = useRef<HTMLFormElement | null>();
+  // handles update from initialization and useEffect
+  const update = useCallback(
+    (form?: HTMLFormElement | null): void => {
+      // mounts
+      if (form) {
+        // filter for input, select, and textarea elements only
+        const supportedElements = Array.from(form.elements).filter<SupportedFormElements>(
+          (element): element is SupportedFormElements => {
+            return (
+              // inputs must be of certain type too
+              (element instanceof HTMLInputElement && !UNSUPPORTED_INPUT_TYPES.has(element.type)) ||
+              element instanceof HTMLSelectElement ||
+              element instanceof HTMLTextAreaElement
+            );
+          },
+        );
+        // add event handlers
+        supportedElements.forEach(ref);
+      } else {
+        // unmount
+        ref(null);
+      }
+    },
+    [ref],
+  );
+  // handle render updates
   useEffect(() => {
-    update();
-    return (): void => ref(null);
-  }, [update, ref]);
-  return (form): void => {
-    testRef.current = form;
-    update();
-  };
+    update(formRef.current);
+    return update;
+  }, [update]);
+  return useCallback(
+    (form): void => {
+      formRef.current = form;
+      update(form);
+    },
+    [formRef, update],
+  );
 }
