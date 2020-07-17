b6724ad590677ffd45464b64846e2c6875139c89
Jay Harris <jay@sourware.com>
2020-07-17 03:10:23 -0400

    Removes useCallback as a solution for nested values

--- src/useFormRef/useFormInputsRef.ts
+++ src/useFormRef/useFormInputsRef.ts
@@ -20,42 +20,25 @@ export function useFormInputsRef({
   const changeRef = useRefEventHandlers({ handlers: handleChange, event: 'change' });
   const blurRef = useRefEventHandlers({ handlers: handleBlur, event: 'blur' });
   const ref = useHandlers(changeRef, blurRef);
-  const formRef = useRef<HTMLFormElement | null>();
-  // handles update from initialization and useEffect
-  const update = useCallback(
-    (form?: HTMLFormElement | null): void => {
-      // mounts
-      if (form) {
-        // filter for input, select, and textarea elements only
-        const supportedElements = Array.from(form.elements).filter<SupportedFormElements>(
-          (element): element is SupportedFormElements => {
-            return (
-              // inputs must be of certain type too
-              (element instanceof HTMLInputElement && !UNSUPPORTED_INPUT_TYPES.has(element.type)) ||
-              element instanceof HTMLSelectElement ||
-              element instanceof HTMLTextAreaElement
-            );
-          },
-        );
-        // add event handlers
-        supportedElements.forEach(ref);
-      } else {
-        // unmount
-        ref(null);
-      }
-    },
-    [ref],
-  );
-  // handle render updates
-  useEffect(() => {
-    update(formRef.current);
-    return update;
-  }, [update]);
-  return useCallback(
-    (form): void => {
-      formRef.current = form;
-      update(form);
-    },
-    [formRef, update],
-  );
+  return (form): void => {
+    // mounts
+    if (form) {
+      // filter for input, select, and textarea elements only
+      const supportedElements = Array.from(form.elements).filter<SupportedFormElements>(
+        (element): element is SupportedFormElements => {
+          return (
+            // inputs must be of certain type too
+            (element instanceof HTMLInputElement && !UNSUPPORTED_INPUT_TYPES.has(element.type)) ||
+            element instanceof HTMLSelectElement ||
+            element instanceof HTMLTextAreaElement
+          );
+        },
+      );
+      // add event handlers
+      supportedElements.forEach(ref);
+    } else {
+      // unmount
+      ref(null);
+    }
+  };
 }
