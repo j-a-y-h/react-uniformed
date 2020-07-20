6d06193f37867c3774644463d767f17b17535584
Jay Harris <jay@sourware.com>
2020-07-19 21:08:28 -0400

    Copies tests from useFormInputsRef

--- test/useFormRef.test.tsx
+++ test/useFormRef.test.tsx
@@ -140,7 +140,163 @@ describe('useFormRef', () => {
       expect(props2[handler]).toBeCalledTimes(1);
     },
   );
-  it.todo('only adds event handler to input, textarea, and select elements');
-  it.todo('will not add event handler to submit type input');
-  it.todo('handles dynamically added input elements');
+  it('only adds event handler to input, textarea, and select elements', async () => {
+    const props = createMockHandlers();
+    const { result } = renderHook(() => useFormRef(props));
+
+    const mount = render(
+      <form ref={result.current.ref}>
+        <label title='name'>Name </label>
+        <input type='text' title='allowed' />
+        <textarea title='allowed'></textarea>
+        <select title='allowed'>
+          <option value='4'>4</option>
+        </select>
+        <object title='name' />
+        <output title='name'>60</output>
+        <fieldset title='name'>
+          <legend title='name'>Choose your favorite monster</legend>
+          <label title='name'>Mothman</label>
+        </fieldset>
+        <button title='name'>Button</button>
+      </form>,
+    );
+    const trigger = async (title = 'name') => {
+      const names = await mount.findAllByTitle(title);
+      names.forEach((name) => {
+        fireEvent(name, new Event('change'));
+        fireEvent(name, new Event('blur'));
+      });
+    };
+    await trigger();
+    expect(props.handleChange).toBeCalledTimes(0);
+    expect(props.handleBlur).toBeCalledTimes(0);
+    await trigger('allowed');
+    expect(props.handleChange).toBeCalledTimes(3);
+    expect(props.handleBlur).toBeCalledTimes(3);
+  });
+  it.each(['submit', 'image', 'reset', 'hidden'])(
+    `will not add event handlers to inputs of '%s' type`,
+    async (type) => {
+      const props = createMockHandlers();
+      const { result } = renderHook(() => useFormRef(props));
+
+      const mount = render(
+        <form ref={result.current.ref}>
+          <input type={type} title='name' />
+        </form>,
+      );
+      const trigger = async (title = 'name') => {
+        const names = await mount.findAllByTitle(title);
+        names.forEach((name) => {
+          fireEvent(name, new Event('change'));
+          fireEvent(name, new Event('blur'));
+        });
+      };
+      await trigger();
+      await trigger();
+      await trigger();
+      expect(props.handleChange).toBeCalledTimes(0);
+      expect(props.handleBlur).toBeCalledTimes(0);
+    },
+  );
+  it('handles dynamically added input elements', async () => {
+    const props = createMockHandlers();
+    const { result, rerender } = renderHook(
+      (props) => {
+        return useFormRef(props);
+      },
+      {
+        initialProps: props,
+      },
+    );
+
+    const Component = ({ anchor, showThird = false }) => (
+      <form ref={anchor}>
+        <input type='text' title='name' />
+        <input type='text' title='name' />
+        {showThird ? <input type='text' title='name' /> : null}
+      </form>
+    );
+
+    expect(props.handleBlur).toBeCalledTimes(0);
+    expect(props.handleChange).toBeCalledTimes(0);
+    const mount = render(<Component anchor={result.current.ref} />);
+    const trigger = async () => {
+      const names = await mount.findAllByTitle('name');
+      names.forEach((name) => {
+        fireEvent(name, new Event('change'));
+        fireEvent(name, new Event('blur'));
+      });
+    };
+    await trigger();
+    expect(props.handleBlur).toBeCalledTimes(2);
+    expect(props.handleChange).toBeCalledTimes(2);
+
+    rerender(props);
+    mount.rerender(<Component anchor={result.current.ref} showThird />);
+    await trigger();
+    expect(props.handleBlur).toBeCalledTimes(5);
+    expect(props.handleChange).toBeCalledTimes(5);
+
+    rerender(props);
+    mount.rerender(<Component anchor={result.current.ref} />);
+    await trigger();
+    expect(props.handleBlur).toBeCalledTimes(7);
+    expect(props.handleChange).toBeCalledTimes(7);
+  });
+  xit('handles dynamically added input elements from nested components', async () => {
+    const props = createMockHandlers();
+    const { result } = renderHook(
+      (props) => {
+        return useFormRef(props);
+      },
+      {
+        initialProps: props,
+      },
+    );
+    const NestedComponent = () => {
+      const [showThird, setShowThird] = React.useState(false);
+      return (
+        <>
+          <button type='button' title='toggle' onClick={() => setShowThird((i) => !i)}>
+            Toggle
+          </button>
+          {showThird ? <input type='text' title='name' /> : null}
+        </>
+      );
+    };
+
+    const Component = ({ anchor }) => (
+      <form ref={anchor}>
+        <input type='text' title='name' />
+        <input type='text' title='name' />
+        <NestedComponent />
+      </form>
+    );
+
+    expect(props.handleBlur).toBeCalledTimes(0);
+    expect(props.handleChange).toBeCalledTimes(0);
+    const mount = render(<Component anchor={result.current} />);
+    const trigger = async () => {
+      const names = await mount.findAllByTitle('name');
+      names.forEach((name) => {
+        fireEvent(name, new Event('change'));
+        fireEvent(name, new Event('blur'));
+      });
+    };
+    await trigger();
+    expect(props.handleBlur).toBeCalledTimes(2);
+    expect(props.handleChange).toBeCalledTimes(2);
+
+    const toggle = await mount.findByTitle('toggle');
+    toggle.click();
+    await trigger();
+    expect(props.handleBlur).toBeCalledTimes(5);
+    expect(props.handleChange).toBeCalledTimes(5);
+    toggle.click();
+    await trigger();
+    expect(props.handleBlur).toBeCalledTimes(7);
+    expect(props.handleChange).toBeCalledTimes(7);
+  });
 });
